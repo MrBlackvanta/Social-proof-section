c9ba8ae8db5367d0e076516b20c5bbef30a8844f
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2022-10-04 13:06:39 +0200

    Update Signature.js

--- src/components/Signature.js
+++ src/components/Signature.js
@@ -2,7 +2,7 @@ import "./Signature.scss";
 function Signature() {
   return (
     <footer class="attribution">
-      <p>
+      <p role="contentinfo">
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           <span> </span>Frontend Mentor
